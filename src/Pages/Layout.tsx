f6e566f1ca2709f426ea1c18523ad3019e7a5251
Max Andersson <maxbjornkga@gmail.com>
2022-11-23 11:29:57 +0100

    React routing

---
+++ src/Pages/Layout.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function StartPage() {
+    return (
+    <Link to="loginPage">
+        <button>Hejsan</button>
+    </Link>
+    
+    )
+}
+
+export default StartPage;
