58ceae6c57b281ea2d370c5846b99e402f03f3df
PettoDavida <maximilian.widman@gmail.com>
2022-11-24 10:45:39 +0100

    Getting theme to work with MUI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.scss";
-import { Button, Typography } from "@mui/material";
+import {Button, Typography} from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button color="primary" variant="contained">
+        <Button color="primary" variant="contained" disabled>
           <Typography fontWeight={"bold"} fontSize={40}>
             test
           </Typography>
