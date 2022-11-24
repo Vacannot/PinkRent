8da8f72fcf2203ac4471abb37f925ef96435274f
Max Andersson <maxbjornkga@gmail.com>
2022-11-24 13:36:07 +0100

    notification component done,

--- src/components/NotificationComponent/NotificationCard.tsx
+++ src/components/NotificationComponent/NotificationCard.tsx
@@ -1,27 +1,61 @@
 import styles from "./NotificationCard.module.scss"
 import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
+import * as React from 'react';
+      import Card from '@mui/material/Card';
+      import CardActions from '@mui/material/CardActions';
+      import CardContent from '@mui/material/CardContent';
+      import Button from '@mui/material/Button';
+      import Typography from '@mui/material/Typography';
+import { IconButton } from "@mui/material";
+import { color } from "@mui/system";
+      
 function NotificationCard() {
     return (
-      <div className={styles.container1} >
-        <div className={styles.container}>
+      <><>
+        <div className={styles.container1}>
+          <div className={styles.container}>
 
-        <div>
-        <p className={styles.name} >Max Andersson</p>
-        <p>Request to rent Ferrari</p>
-        </div>
-        <div className={styles.icons} >
-            <div className={styles.colorGreen} >
-            < CheckIcon />
+            <div>
+              <p className={styles.name}>Max Andersson</p>
+              <p className={styles.margin} >Request to rent Ferrari</p>
             </div>
-             <div className={styles.colorRed} >
-            < CloseIcon />
-             </div>
-
+            <div>
+            <IconButton aria-label="delete"  color="success"  >
+              <CheckIcon />
+            </IconButton>
+            <IconButton aria-label="delete"  color="error"   >
+              <CloseIcon/>
+            </IconButton>
+            </div>
+          </div>
+             <hr className={styles.line}></hr>
         </div>
-        </div>
-           <hr className={styles.line} ></hr>
-      </div>
+      </>
+      <Card className="cardComponent" sx={{ width: "80%", display: "flex", mx: "auto",
+        '@media screen and (max-width: 650px)': { display: 'none',}, }}>
+          
+          <CardContent sx={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "space-around" }}>
+            <img src="Rectangle42.png" alt="" />
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Max Andersson
+            </Typography>
+            <CardActions>
+            <IconButton aria-label="delete"  color="success"  >
+              <CheckIcon />
+            </IconButton>
+            <IconButton aria-label="delete"  color="error"   >
+              <CloseIcon/>
+            </IconButton>
+            </CardActions>
+          </CardContent>
+        </Card>
+        </>
+      
+            
      )
     }
 
