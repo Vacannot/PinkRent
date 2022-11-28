8b9b9dd8da68f2521ff8295f56f10a1f2b23f3f8
Simon Eriksson <44813993+Vacannot@users.noreply.github.com>
2022-11-28 03:38:36 +0100

    background waves

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,13 +1,16 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
+import Wave from "../components/Wave/Wave";
 
 function Layout() {
   window.scrollTo(0, 0);
-  const navigate = useNavigate();
   return (
     <div>
       <Header />
-      <Outlet />
+      <Wave />
+      <>
+        <Outlet />
+      </>
     </div>
   );
 }
