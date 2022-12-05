87d11f1e2e526548f462d4c90ca2b980ee340319
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2022-12-05 14:13:50 +0100

    center login & register form

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,18 @@
+import { Box } from "@mui/material";
 import LoginForm from "../components/LoginForm/LoginForm";
 
 function LoginPage() {
-  return <LoginForm />;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        marginBottom: "5rem",
+      }}
+    >
+      <LoginForm />
+    </Box>
+  );
 }
 
 export default LoginPage;
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,7 +1,18 @@
+import { Box } from "@mui/material";
 import RegisterForm from "../components/RegisterForm/RegisterForm";
 
 function RegisterPage() {
-  return <RegisterForm />;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        marginBottom: "5rem",
+      }}
+    >
+      <RegisterForm />
+    </Box>
+  );
 }
 
 export default RegisterPage;
