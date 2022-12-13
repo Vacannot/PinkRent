a7718fc115a84d4ee1840b574c5c2f10e2de57e7
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2022-12-13 18:06:45 +0100

    tried

--- src/pages/productUserPage.tsx
+++ src/pages/productUserPage.tsx
@@ -4,38 +4,68 @@ import { useParams } from "react-router";
 import { useAuth } from "../backend/Context";
 
 function ProductUserPage() {
-    const {getProductsByUserID } = useAuth();
-    const [products, setProducts] = useState<any[]>([]);
-    let params = useParams();
-    const userID = params.userID;
-  
-    useEffect(() => {
-      let product = getProductsByUserID(userID!);
-      product.then((data) => {
-        console.log(data);
-        setProducts(data);
-      });
-  
-    }, [userID, getProductsByUserID]);
-    return (
-        <>
-        <Box sx={{display:'flex', flexDirection:'column', width:"100%", transform: 'translate(25%,-25%)'}}>
-          
+  const { getProductsByUserID } = useAuth();
+  const [products, setProducts] = useState<any[]>([]);
+  let params = useParams();
+  const userID = params.userID;
+
+  useEffect(() => {
+    let product = getProductsByUserID(userID!);
+    product.then((data) => {
+      console.log(data);
+      setProducts(data);
+    });
+  }, [userID, getProductsByUserID]);
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          mt: "6rem",
+          transform: "translate(15%,-15%)",
+          maxWidth: "65rem",
+        }}
+      >
+        <Typography
+          sx={{ mb: "5rem", fontSize: "1.5rem", textAlign: "center" }}
+        >
+          Products
+        </Typography>
         {products.map((item) => {
-            return (
-               
-            <Box sx={{ width:"100%",  justifyContent:"center",  marginBottom:"1rem" }}  >
-              <Box sx={{ width:"70%", display:"flex", alignItems:"end"}}  >
-                <img  style={{ width:"7rem", height:"7rem"  }} src={item.image} alt="" />
-                 <Typography>{item.title}</Typography>
-                 <Typography>{item.price} kr</Typography>
+          return (
+            <Box
+              sx={{
+                width: "100%",
+                justifyContent: "center",
+                marginBottom: "2rem",
+                maxWidth: "65rem",
+              }}
+            >
+              <Box sx={{ width: "70%", display: "flex", alignItems: "end" }}>
+                <img
+                  style={{ width: "7rem", height: "7rem" }}
+                  src={item.image}
+                  alt=""
+                />
+                <Typography>{item.title}</Typography>
+                <Typography>{item.price} kr</Typography>
               </Box>
-                 <Box sx={{ backgroundColor:"gray", width:"70%", height:".1rem", justifyContent:"center" }}  ></Box>
-            </Box>);
+              <Box
+                sx={{
+                  backgroundColor: "gray",
+                  width: "70%",
+                  height: ".1rem",
+                  justifyContent: "center",
+                }}
+              ></Box>
+            </Box>
+          );
         })}
-        </Box>
-        </>
-    )
+      </Box>
+    </>
+  );
 }
 
-export default ProductUserPage
+export default ProductUserPage;
