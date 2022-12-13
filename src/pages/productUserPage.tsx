6ed9828315fb90d76a67186b7acb15ab0190acc6
Max Andersson <maxbjornkga@gmail.com>
2022-12-13 16:48:51 +0100

    layout not done
    Co-authored-by: Maximilian Widman <PettoDavida@users.noreply.github.com>
    Co-authored-by: Fia <fiababiakandersson@users.noreply.github.com>

--- src/pages/productUserPage.tsx
+++ src/pages/productUserPage.tsx
@@ -1,8 +1,40 @@
-import UserProductPage from "../components/userProfile/UserProductPage"
+import { Box, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { useAuth } from "../backend/Context";
 
 function ProductUserPage() {
+    const {getProductsByUserID } = useAuth();
+    const [products, setProducts] = useState<any[]>([]);
+    let params = useParams();
+    const userID = params.userID;
+  
+    useEffect(() => {
+      let product = getProductsByUserID(userID!);
+      product.then((data) => {
+        console.log(data);
+        setProducts(data);
+      });
+  
+    }, [userID, getProductsByUserID]);
     return (
-        <UserProductPage />
+        <>
+        <Box sx={{display:'flex', flexDirection:'column', width:"100%", transform: 'translate(25%,-25%)'}}>
+          
+        {products.map((item) => {
+            return (
+               
+            <Box sx={{ width:"100%",  justifyContent:"center",  marginBottom:"1rem" }}  >
+              <Box sx={{ width:"70%", display:"flex", alignItems:"end"}}  >
+                <img  style={{ width:"7rem", height:"7rem"  }} src={item.image} alt="" />
+                 <Typography>{item.title}</Typography>
+                 <Typography>{item.price} kr</Typography>
+              </Box>
+                 <Box sx={{ backgroundColor:"gray", width:"70%", height:".1rem", justifyContent:"center" }}  ></Box>
+            </Box>);
+        })}
+        </Box>
+        </>
     )
 }
 
