8a9a569f7233f260c9bb12c681cf3fbf009b79b4
Simon Eriksson <44813993+Vacannot@users.noreply.github.com>
2022-12-14 02:17:27 +0100

    notifications update w bug fixes, no "no notfications fix" yet (#76)
    * notifications update w bug fixes, no backend yet
    
    * made it so products that are rented cannot be clicked
    
    * fixes for "free" vs 0 kr on produkt card
    
    * fixed so that createNotificatin now calls correctly from both detailed view and catalog view
    
    Co-authored-by: vacannot <simon.eriksson@hiq.se>

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,17 +1,17 @@
-import {useEffect, useState} from "react";
-import {useParams} from "react-router";
-import {useAuth} from "../backend/Context";
+import { useMemo, useState } from "react";
+import { useParams } from "react-router";
+import { useAuth } from "../backend/Context";
 import ProductDetails from "../components/ProductDetails/ProductDetails";
 
 function ProductDetailPage() {
-  const {getProductByID} = useAuth();
+  const { getProductByID } = useAuth();
 
   const [product, setProduct] = useState<any>();
 
   let params = useParams();
   const productID = params.productID;
 
-  useEffect(() => {
+  useMemo(() => {
     let product = getProductByID(productID!);
     product.then((data) => {
       console.log(data);
@@ -19,7 +19,11 @@ function ProductDetailPage() {
     });
   }, [productID, getProductByID]);
 
-  return <>{product ? <ProductDetails product={product} /> : <p>321</p>}</>;
+  return (
+    <>
+      {product ? <ProductDetails product={product} /> : <p>No Details found</p>}
+    </>
+  );
 }
 
 export default ProductDetailPage;
