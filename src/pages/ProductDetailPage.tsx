2b4224fe84418626e34afbb51e7c5b01b2deb0a8
vacannot <simon.eriksson@hiq.se>
2022-12-15 00:10:16 +0100

    fixed changes on productdetailspage

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -2,26 +2,37 @@ import { useMemo, useState } from "react";
 import { useParams } from "react-router";
 import { useAuth } from "../backend/Context";
 import ProductDetails from "../components/ProductDetails/ProductDetails";
+import { Box, Typography } from "@mui/material";
 
 function ProductDetailPage() {
-  const {getProductByID } = useAuth();
+  const { getProductByID } = useAuth();
   const [product, setProduct] = useState<any>();
   let params = useParams();
   const productID = params.productID;
-  
-  
 
   useMemo(() => {
     let product = getProductByID(productID!);
     product.then((data) => {
       setProduct(data);
     });
-
   }, [productID, getProductByID]);
 
-
-
-  return <>{product ? <ProductDetails product={product}  /> : <p>321</p>}</>;
+  return (
+    <>
+      {product ? (
+        <ProductDetails product={product} />
+      ) : (
+        <Box
+          sx={{
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        >
+          <Typography variant="h4">Loading Details</Typography>
+        </Box>
+      )}
+    </>
+  );
 }
 
 export default ProductDetailPage;
