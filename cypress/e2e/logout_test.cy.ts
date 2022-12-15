b61f296efa6a6e1da046449892a2b97b7f0de27f
PettoDavida <maximilian.widman@gmail.com>
2022-12-15 14:45:52 +0100

    test2

--- cypress/e2e/logout_test.cy.ts
+++ cypress/e2e/logout_test.cy.ts
@@ -1,9 +1,13 @@
 import * as authUser from "../fixtures/auth-user.json";
 describe("The Logout Test", () => {
   it("Should logout", () => {
-    const {name} = authUser;
+    const {email, password, name} = authUser;
     cy.visit("/");
-
+    cy.contains("login").click();
+    cy.get("input[type=email]").type(email);
+    cy.get("input[type=password]").type(password);
+    cy.get("button[type=submit]").click();
+    cy.contains(name).should("exist");
     cy.contains(name).click();
     cy.contains("Logout").click();
     cy.contains("login").should("exist");
--- cypress/e2e/profilepage_test.cy.ts
+++ cypress/e2e/profilepage_test.cy.ts
@@ -1,9 +1,13 @@
 import * as authUser from "../fixtures/auth-user.json";
 describe("The profile page Test", () => {
   it("Checks for profilepage", () => {
-    const {name} = authUser;
+    const {email, password, name} = authUser;
     cy.visit("/");
-
+    cy.contains("login").click();
+    cy.get("input[type=email]").type(email);
+    cy.get("input[type=password]").type(password);
+    cy.get("button[type=submit]").click();
+    cy.contains(name).should("exist");
     cy.contains(name).click();
     cy.contains("Name:").should("exist");
     cy.contains("Email:").should("exist");
